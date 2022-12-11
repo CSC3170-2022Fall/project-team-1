a7eeb2bd92d98b65616cec0425dac7a759f33648
michaelGGGL <106070966+michaelGGGL@users.noreply.github.com>
2022-12-11 20:49:51 +0800

    Add files via upload

--- consumer-login.php
+++ consumer-login.php
@@ -1,15 +1,61 @@
 <html>
 	<head>
-		<title>My first PHP website</title>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+		<title>consumer login website</title>
+		<link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700&amp;display=swap">
+		<link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
 	</head>
 	<body>
-		<h2>Login Page</h2>
-		<a href="index.php">Click here to go back</a><br/><br/>
-		<form action="consumer-login.php" method="post">
-			Enter consumer_name: <input type="text" name="consumer_name" required="required"/> <br/>
-			Enter Password: <input type="password" name="password" required="required" /> <br/>
-			<input type="submit" value="Login"/>
-		</form>
+		<nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
+			<div class="container"><a class="navbar-brand logo" href="#">Chip Land</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navbarNav"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="projects-grid-cards.html">Projects</a></li>
+						<li class="nav-item"><a class="nav-link" href="consumer-login.php">I'm a consumer</a></li>
+						<li class="nav-item"><a class="nav-link" href="plant-login.php">I'm a plant owner</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<main class="page project-page">
+			<section class="portfolio-block project">
+				<div class="container">
+					<div class="heading">
+						<h2>Consumer sign in</h2>
+					</div>
+					<div class="row">
+						<div class="col-12 col-md-6 offset-md-1 info">
+						
+						<form action="consumer-login.php" method="post">
+						<div class="mb-3">
+							<label for="exampleInputEmail1" class="form-label">Consumer name</label>
+							<input type="text" class="form-control" name="consumer_name" required="required">
+							
+						</div>
+						<div class="mb-3">
+							<label for="exampleInputPassword1" class="form-label">Password</label>
+							<input type="password" class="form-control" name="password" required="required">
+						</div>
+						<button type="submit" class="btn btn-primary" value="Login">Login</button>  <a href="consumer-register.php">Sign Up</a>
+
+						</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		<footer class="page-footer">
+			<div class="container">
+			<div class="links"><a href="consumer-register.php">Sign Up</a><a href="index.php">Home</a><a href="#">Login</a></div>
+			</div>
+		</footer>
+		<script src="assets/bootstrap/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
+		<script src="assets/js/theme.js"></script>
 	</body>
 </html>
 
@@ -45,7 +91,7 @@
 			}
 		} else {
 			print '<script>alert("Incorrect consumer_name!");</script>'; //Prompts the user
-			print '<script>window.location.assign("index.php");</script>'; // redirects to login.php
+			print '<script>window.location.assign("consumer-login.php");</script>'; // redirects to login.php
 		}
 	}
 ?>
--- plant-login.php
+++ plant-login.php
@@ -1,15 +1,67 @@
 <html>
 	<head>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 		<title>plant login website</title>
+		<link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700&amp;display=swap">
+		<link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
 	</head>
 	<body>
-		<h2>Login Page</h2>
-		<a href="index.php">Click here to go back</a><br/><br/>
-		<form action="plant-login.php" method="post">
-			Enter plant_name: <input type="text" name="plant_name" required="required"/> <br/>
-			Enter Password: <input type="password" name="password" required="required" /> <br/>
-			<input type="submit" value="Login"/>
-		</form>
+		
+		
+		<nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
+			<div class="container"><a class="navbar-brand logo" href="#">Chip Land</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navbarNav"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="projects-grid-cards.html">Projects</a></li>
+						<li class="nav-item"><a class="nav-link" href="consumer-login.php">I'm a consumer</a></li>
+						<li class="nav-item"><a class="nav-link" href="plant-login.php">I'm a plant owner</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<main class="page project-page">
+			<section class="portfolio-block project">
+				<div class="container">
+					<div class="heading">
+						<h2>Plant sign in</h2>
+					</div>
+					<div class="row">
+						<div class="col-12 col-md-6 offset-md-1 info">
+						
+						<form action="plant-login.php" method="post">
+							<!--Enter plant_name: <input type="text" name="plant_name" required="required"/> <br/>
+							Enter Password:   <input type="password" name="password" required="required" /> <br/>
+							<input type="submit" value="Login"/>-->
+						
+						<div class="mb-3">
+							<label for="exampleInputEmail1" class="form-label">Plant name</label>
+							<input type="text" class="form-control" name="plant_name" required="required">
+							
+						</div>
+						<div class="mb-3">
+							<label for="exampleInputPassword1" class="form-label">Password</label>
+							<input type="password" class="form-control" name="password" required="required">
+						</div>
+						<button type="submit" class="btn btn-primary" value="Login">Login</button>  <a href="plant-register.php">Sign Up</a>
+
+						</form>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		<footer class="page-footer">
+			<div class="container">
+			<div class="links"><a href="plant-register.php">Sign Up</a><a href="index.php">Home</a><a href="#">Login</a></div>
+			</div>
+		</footer>
+		<script src="assets/bootstrap/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
+		<script src="assets/js/theme.js"></script>
 	</body>
 </html>
 
@@ -44,7 +96,7 @@
 				print '<script>window.location.assign("plant-login.php");</script>'; // redirects to login.php
 			}
 		} else {
-			print '<script>alert("Incorrect consumer_name!");</script>'; //Prompts the user
+			print '<script>alert("Incorrect plant name!");</script>'; //Prompts the user
 			print '<script>window.location.assign("plant-login.php");</script>'; // redirects to login.php
 		}
 	}
