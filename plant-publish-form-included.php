651d25be2846579381e21982ac4fe4a86215a75a
Annaaaa997 <726827048@qq.com>
2022-12-24 21:46:02 +0800

    publish ui

--- plant-publish-form-included.php
+++ plant-publish-form-included.php
@@ -1,5 +1,7 @@
-<div class="col-lg-3"></div>
-<div class="col-lg-6">
+
+
+<div class="col-lg-2" id="big"></div>
+<div class="col-lg-8" id="aaa">
     <div class="mt-4">
 
 
@@ -9,14 +11,14 @@
 
             <div class="row mb-4">
                 <label for="horizontal-firstname-input" class="col-sm-3 col-form-label">Model name</label>
-                <div class="col-sm-9">
+                <div class="col-sm-6">
                     <input type="text" class="form-control" id="horizontal-firstname-input" name="machine_model_name"
                         required="required" />
                 </div>
             </div>
             <div class="row mb-4">
                 <label for="horizontal-firstname-input" class="col-sm-3 col-form-label">Machine number</label>
-                <div class="col-sm-9">
+                <div class="col-sm-6">
                     <input type="range" min="100" max="10000" value="100"
                         oninput="this.nextElementSibling.value = this.value" class="form-control"
                         id="horizontal-firstname-input" name="machine_model_num" required="required" />
@@ -24,31 +26,45 @@
                 </div>
             </div>
 
-            <div class="row justify-content-end">
-                <table border='2px'>
-                    <th>Chip Model</th>
-                    <th>Operation Type</th>
-                    <th>Time</th>
-                    <th>Expense</th>
-                    <?php
-                    $operation_types = $mysqli->query("SELECT `chip_model`, `operation_type` FROM Operation_Types ORDER BY `chip_model`, `priority`");
-                    $name = 1;
-                    $time = 10;
-                    $expense = 100;
-                    while ($operation_type_row = mysqli_fetch_array($operation_types)) {
-                        echo '<tr>
-                        <td align="center">' . "$operation_type_row[chip_model]" . '</td>
-                        <td>' . "$operation_type_row[operation_type]" . '</td>
-                        <td>' . "<input type='range' min='1' max='100' value='$time' oninput='this.nextElementSibling.value = this.value' name='$name'/>" . "<output>$time</output>" . '</td>';
-                        $name++;
-                        $time = $time + 5;
-                        echo '<td>' . "<input type='range' min='10' max='1000' value='$expense' oninput='this.nextElementSibling.value = this.value' name='$name'/>" . "<output>$expense</output>" . '</td>';
-                        $name++;
-                        $expense = $expense + 50;
-                        echo '</tr>';
-                    }
-                    ?>
-                </table>
+            <div class="row justify-content-end col-md-28" id="bbb">
+                <!-- editable tables -->
+                <!-- ----------      start        -------------- -->
+                <div class="table-responsive" id = "ttt">
+                    <table class="table table-editable table-nowrap align-middle table-edits">
+                        <thead>
+                            <tr>
+                            <th>Chip Model</th>
+                            <th>Operation Type</th>
+                            <th>Time</th>
+                            <th>Expense</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+                        <?php
+                            $operation_types = $mysqli->query("SELECT `chip_model`, `operation_type` FROM Operation_Types ORDER BY `chip_model`, `priority`");
+                            $name = 1;
+                            $time = 10;
+                            $expense = 100;
+                            while ($operation_type_row = mysqli_fetch_array($operation_types)) {
+                            echo '<tr>
+                            <td data-field="Chip Model" style="width: 80px">' . "$operation_type_row[chip_model]" . '</td>
+                            <td data-field="Operation Type">' . "$operation_type_row[operation_type]" . '</td>
+                            <td data-field="Time">' . "<input type='range' min='1' max='100' value='$time' oninput='this.nextElementSibling.value = this.value' name='$name'/>" . "<output>$time</output>" . '</td>';
+                            $name++;
+                            $time = $time + 5;
+                            echo '<td data-field="Expense">' . "<input type='range' min='10' max='1000' value='$expense' oninput='this.nextElementSibling.value = this.value' name='$name'/>" . "<output>$expense</output>" . '</td>';
+                            $name++;
+                            $expense = $expense + 50;
+                            echo '</tr>';
+                            }
+                        ?>
+
+                        </tbody>
+                        </table>
+                </div>
+                <!-- editable tables -->
+                <!-- -------      end     -------- -->
                 <div class="col-sm-9" ;>
                     <button type="submit" style="margin-top:20px; margin-left:120px; font-size:larger;"
                         class="btn btn-primary w-md">Publish</button>
