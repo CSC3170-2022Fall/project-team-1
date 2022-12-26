e7476d8efdb4bb3aa34a0a10aaf4edc68dfb0210
Ary <90801772+arydinesen@users.noreply.github.com>
2022-12-27 00:25:02 +0800

    Update c-signin.php

--- c-signin.php
+++ c-signin.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		print '<script>window.location.assign("c-appoint.php");</script>';
 	} else {
 		print '<script>alert("Username or password incorrect!");</script>';
-		print '<script>window.location.assign("c-login.php");</script>';
+		print '<script>window.location.assign("c-signin.php");</script>';
 	}
 }
 ?>
